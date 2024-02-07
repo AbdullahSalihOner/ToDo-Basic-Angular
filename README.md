bdd767bd007266f01cec801d120c2d14dfa379ed
Abdullah Salih Öner <riyadlioner00@gmail.com>
2024-02-07 15:55:21 +0300

    FirstCommit

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
+# Basic ToDo Project
+ this 
+
+
+
 # ToDoProject
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 17.1.2.
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,45 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
+import { FormsModule } from '@angular/forms';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatButtonModule } from '@angular/material/button';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatIconModule,FormsModule,MatCheckboxModule,MatButtonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'ToDo-Project';
+  title = 'todo-list';
+
+  inputText: string = "";
+
+  todos: string[] = [];
+  deletedTodos: string[] = [];
+
+ addTodo() {
+   this.todos.push(this.inputText);
+    this.inputText = ' ';
+    console.log(this.todos);
+ }
+
+ isEdit: boolean = false;
+
+ deleteTodo(index: number) {
+   this.deletedTodos.push(this.todos[index]);
+   this.todos.splice(index, 1);
+ }
+
+  editTodo(index: number) {
+   
+    let newInput = prompt("Edit Todo", "");  
+    console.log(newInput);  
+    if (newInput !== null) { this.todos[index] = newInput; }
+    if(!this.isEdit) this.isEdit = true; 
+    else this.isEdit = false;
+  }
 }
